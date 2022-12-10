7e590c957e59c05aec250dec5e2b12b7fa525c78
Aboubakary833 <aboubakarycisse472@gmail.com>
2022-12-10 19:57:49 +0000

    (change): Firebase backend added

---
+++ js/config.example.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "bf-day",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+}
+
+export default config
